1eb5415ddb2b0c45c97238045fce8f1f19d81eba
Yusef Yassin-Morales <yusefm97@icloud.com>
2021-08-23 22:44:29 -0600

    refactor: made all import lines into a single line

--- bikeshare.py
+++ bikeshare.py
@@ -1,6 +1,4 @@
-import time
-import pandas as pd
-import numpy as np
+import time, pandas as pd, numpy as np
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
